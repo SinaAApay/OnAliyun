4c0b8eb4dc74d23ec762c43519e904ba25a775e9
SinaAApay <lyl_hit@163.com>
2014-10-28 22:54:07 +0800

    firstcommit

---
+++ activity.py
@@ -0,0 +1,108 @@
+import os,sys
+path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(path)
+import web
+import pymongo
+import code
+
+render=web.template.render('/var/www/AA/static')
+con=pymongo.Connection('localhost',27017)
+db=con.aapay
+activities=db.activities
+users=db.users
+
+class currentActivity:
+    def GET(self):
+#	return "hello"
+        cookie=web.cookies()
+        activityIn=[]
+        activityORG=[]
+        uid=int(cookie[u'uid'])
+        user=users.find_one({'uid':uid})
+        if user!=None:
+            for ac in user[u'activities']:
+                a_activity=activities.find_one({u'weibo_id':ac})
+                if a_activity[u'ifend']==False and a_activity[u'ifclose']==False:
+                    activityORG.append(a_activity)
+        name=cookie[u'screen_name']
+        for ac in activities.find():
+            if name in ac[u'peopleInvited']:
+                if ac[u'ifend']==False and ac[u'ifclose']==False and (ac in  activityORG)==False:
+                    activityIn.append(ac)
+
+        return render.currentActivity(activityIn,activityORG)
+
+
+class pastActivity:
+    def GET(self):
+        cookie=web.cookies()
+        activityIn=[]
+        activityORG=[]
+        uid=int(cookie[u'uid'])
+        user=users.find_one({'uid':uid})
+        if user!=None:
+            for ac in user[u'activities']:
+                a_activity=activities.find_one({u'weibo_id':ac})
+                if a_activity[u'ifend']==True:
+                    activityORG.append(a_activity)
+        name=cookie[u'screen_name']
+        for ac in activities.find():
+            if name in ac[u'peopleIn']:
+                if ac[u'ifend']==True:
+                    if (ac in activityORG)==False:
+                        activityIn.append(ac)
+
+        return render.pastActivity(activityIn,activityORG)
+
+class startActivity:
+    def POST(self):
+        webinput=web.input()
+        weibo_id=webinput[u'weibo_id']
+        ac=activities.find_one({u'weibo_id':weibo_id})
+        ifbegin=True
+        for people in ac[u'peopleInvited']:
+            if (people in ac[u'peopleIn'])==False:
+                ifbegin=False
+                break
+        if ifbegin:
+            activities.update({u'weibo_id':weibo_id},{'$set':{u'ifbegin':True}})
+            web.seeother("/currentActivity")
+        else:
+            web.seeother("/currentActivity")
+
+
+
+class endActivity:
+    def POST(self):
+        webinput=web.input()
+        weibo_id=webinput[u'weibo_id']
+        ac=activities.find_one({u'weibo_id':weibo_id})
+        if ac[u'ifbegin']==True:
+            activities.update({u'weibo_id':weibo_id},{"$set":{u'ifend':True}})
+            web.seeother("/pastActivity")
+        else:
+            web.seeother("/currentActivity")
+
+class attendActivity:
+    def POST(self):
+        webinput=web.input()
+        weibo_id=webinput[u'weibo_id']
+        ac=activities.find_one({u'weibo_id':weibo_id})
+        cookies=web.cookies()
+        name=cookies[u'screen_name']
+        if name in ac[u'peopleIn']:
+            web.seeother("/currentActivity")
+        else:
+            peopleIn=ac[u'peopleIn']
+            peopleIn.append(name)
+            activities.update({u'weibo_id':weibo_id},{"$set":{u'peopleIn':peopleIn}})
+            web.seeother("/currentActivity")
+
+
+class refuseActivity:
+    def POST(self):
+        webinput=web.input()
+        weibo_id=webinput[u'weibo_id']
+        activities.update({u'weibo_id':weibo_id},{"$set":{u'ifclose':True}})
+        web.seeother("/currentActivity")
+
---
+++ code.py
@@ -0,0 +1,110 @@
+import os,sys
+path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(path)
+import web
+import urllib,urllib2
+import weibo
+import pachong
+import newclass
+import activity
+from getclient import getClient
+urls=(
+        "/","index",
+        "/url","askforusercode",
+        "/redirecturl","getCodeSetCookie",
+        "/beginActivity","designActivity",
+        "/error","error",
+        "/sendBeginWeibo","newclass.sendweibo",
+        "/currentActivity","activity.currentActivity",
+        "/pastActivity","activity.pastActivity",
+        "/userindex","userindex",
+        "/startActivity","activity.startActivity",
+        "/endActivity","activity.endActivity",
+        "/attendActivity","activity.attendActivity",
+        "/refuseActivity","activity.refuseActivity",
+        "/us","us"
+
+    )
+
+render=web.template.render('/var/www/AA/static')
+
+app=web.application(urls,globals())
+application=app.wsgifunc()
+def getAccesstokenAndSoOn(code):
+    #print code
+    appkey='1063052666'
+    appsecret='c7d48410f4c03b87d5319edf5d544fd6'
+    redirecturl='http://123.57.11.233/AA/redirecturl'
+    client=weibo.APIClient(app_key=appkey,app_secret=appsecret,redirect_uri=redirecturl)
+    r=client.request_access_token(str(code[u'code']))
+    web.setcookie('uid',r.uid)
+    web.setcookie('access_token',r.access_token)
+    web.setcookie('expires_in',r.expires_in)
+    client.set_access_token(r.access_token,r.expires_in)
+    userinfor={}
+    userinfor[u'uid']=r.uid
+    userinfor[u'client']=client
+    return userinfor
+
+class us:
+    def GET(self):
+        return render.us()
+
+
+
+class error:
+    def GET(self):
+        return "Sorry,some errors happened,please email us weiyanjie@gmail.com"
+
+class index:
+    def GET(self):
+        return render.index();
+ 
+class askforusercode:
+    def GET(self):
+        web.seeother("https://api.weibo.com/oauth2/authorize?client_id=1063052666&redirect_uri=http://123.57.11.233/AA/redirecturl&response_type=code")
+
+
+class getCodeSetCookie:
+    def GET(self):
+        code=web.input(code="")
+        if len(code[u'code'])>1:
+            userinfor=getAccesstokenAndSoOn(code)
+            infor=userinfor[u'client'].users.show.get(uid=int(userinfor[u'uid']))
+            web.setcookie('screen_name',infor[u'screen_name'])
+        
+        web.seeother("/userindex")
+
+class userindex:
+    def GET(self):
+        cookies=web.cookies()
+        ac=cookies[u'access_token']
+        expires_in=cookies[u'expires_in']
+        client=getClient(ac,expires_in)
+        infor=client.users.show.get(uid=int(cookies[u'uid']))
+        return render.userindex(infor)
+
+class designActivity:
+    def GET(self):
+        if web.cookies()!=None:
+            cookies=web.cookies()
+            p=pachong.pachong()
+            friends=[]
+            i=2
+            try:
+                friends=p.getBiFriendsName(uid=int(cookies.uid),i=1)
+                while len(p.getBiFriendsName(uid=int(cookies.uid),i=i))>90:
+                    friends+=p.getBiFriendsName(uid=int(cookies.uid),i=i)
+                    i+=1
+            except:
+                web.SeeOther("/error")
+
+            web.setcookie("friendscount",len(friends))
+            return render.designActivity(friends)
+        else:
+            web.seeother("https://api.weibo.com/oauth2/authorize?client_id=1063052666&redirect_uri=http://123.57.11.233/AA/redirecturl&response_type=code")
+
+
+
+if __name__=="__main__":
+    app.run()
